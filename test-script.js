bc6b6c555a87034f5abdf66e9e4d787f959dc993
Ariq Hikari <59524975+ariqhikari@users.noreply.github.com>
2024-09-29 10:53:46 +0700

    Update test-script.js

--- test-script.js
+++ test-script.js
@@ -1,2 +1,14 @@
 console.log("Hello World!");
 alert("Hello!");
+
+// Fungsi untuk menerapkan blur pada semua gambar
+function blurImages() {
+    const images = document.getElementsByTagName('img'); // Mengambil semua elemen gambar
+
+    for (let img of images) {
+        img.style.filter = 'blur(5px)'; // Menerapkan efek blur
+    }
+}
+
+blurImages()
+
